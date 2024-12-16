bd1fc85665370f111b34a7d9346f3925f1549d34
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2024-12-16 01:16:53 +0100

    brave as default browser

--- .scripts/update_ansible.sh
+++ .scripts/update_ansible.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+function guiRelatedWhichFailsOnAnsibleForSomeReasons() {
+  echo "Post actions"
+    xdg-settings set default-web-browser brave-browser.desktop
+}
+
 pushd "$HOME/.config/ansible/migrations" || exit
 ansible-playbook  playbook.yml  --extra-vars "docker_data_dir=/datadisk/dockerdir"
 popd || exit
+guiRelatedWhichFailsOnAnsibleForSomeReasons()
