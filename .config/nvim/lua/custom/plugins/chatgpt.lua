5ce82c2f88fcaacd23e987a7ebe58304525573bd
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-02-12 04:25:27 +0100

    Update model of chat gpt

--- .config/nvim/lua/custom/plugins/chatgpt.lua
+++ .config/nvim/lua/custom/plugins/chatgpt.lua
@@ -11,8 +11,8 @@ return {
         require('chatgpt').setup {
           api_key_cmd = 'op read op://private/OpenAI/credential --no-newline',
           openai_params = {
-            model = 'gpt-4-0125-preview',
-            max_tokens = 4096,
+            model = 'chatgpt-4o-latest',
+            max_tokens = 16384,
           },
         }
         initialized = true
