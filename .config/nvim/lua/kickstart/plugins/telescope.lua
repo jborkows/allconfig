69d335eab8c7cd15f722acd5186f207e317a3a97
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-02-14 17:02:56 +0100

    Find symbol in file

--- .config/nvim/lua/kickstart/plugins/telescope.lua
+++ .config/nvim/lua/kickstart/plugins/telescope.lua
@@ -83,6 +83,7 @@ return {
       vim.keymap.set('n', '<leader>fa', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = '[S]erch for symbols in current file' })
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
