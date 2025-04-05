48c0ebb4625a5a4b26a81f98d354f86f603455aa
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-04-05 10:12:01 +0200

    Python injection

--- .config/nvim/queries/make/injections.scm
+++ .config/nvim/queries/make/injections.scm
@@ -1,9 +1,9 @@
 (
-(raw_text) @python (#match? @python "python3 -c")
-  (#offset! @python 0 12)
+  (recipe_line
+    (shell_text) @python)
+  (#match? @python "^python3 -c \"")
+  (#set! injection.language "python")
+  (#set! injection.include-quoted)
+  (#offset! @python 12 0 -1 0)
+)
 
-  );
-
-(
-(shell_text) @bash
- );
