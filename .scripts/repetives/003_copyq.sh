930e888c4616d08b9e79cbacac955fc026026d59
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-08-16 00:27:17 +0200

    Install copyq

---
+++ .scripts/repetives/003_copyq.sh
@@ -0,0 +1,53 @@
+copyq_install(){
+    required(){
+        sudo apt install \
+          cmake \
+          extra-cmake-modules \
+          git \
+          libqt5svg5 \
+          libqt5svg5-dev \
+          libqt5waylandclient5-dev \
+          libqt5x11extras5-dev \
+          libwayland-dev \
+          libxfixes-dev \
+          libxtst-dev \
+          qtbase5-private-dev \
+          qtdeclarative5-dev \
+          qttools5-dev \
+          qttools5-dev-tools \
+          qtwayland5 \
+          qtwayland5-dev-tools \
+          libkf5notifications-dev
+    }
+
+    install(){
+        required
+        (
+            cd /tmp
+            git clone https://github.com/hluk/CopyQ.git
+            cd CopyQ
+            cmake .
+            make
+            cp copyq ~/programs
+        )
+    }
+    VERSION_LATEST=$(curl -Ls -o /dev/null -w "%{url_effective}\n"  https://github.com/hluk/CopyQ/releases/latest)
+    VERSION=${VERSION_LATEST##*/}
+
+    if ! which copyq >> /dev/null; then
+        install
+        return
+    fi
+    current_version=$(copyq --version | head -n 1 | cut -f4 -d' ' | cut -f1 -d'-')
+    if [[ $VERSION != "$current_version" ]]; then
+        echo installing due latest version "${VERSION}" current "$current_version"
+        install
+        return
+    fi
+
+
+}
+
+copyq_install
+
+
--- .scripts/update_ansible.sh
+++ .scripts/update_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-bash $SCRIPT_DIR/repetives/init.sh "$@"
+bash "$SCRIPT_DIR/repetives/init.sh" "$@"
