a69c63cf46edd456e3c5cd988dc0066eb644dd5d
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2026-04-03 20:17:03 +0200

    Install tree sitter

---
+++ .scripts/repetives/002_treesitter.sh
@@ -0,0 +1,31 @@
+fetch(){
+    (
+        cd /tmp
+        curl -L -o tree-sitter-cli-linux-x64.zip https://github.com/tree-sitter/tree-sitter/releases/download/v$1/tree-sitter-cli-linux-x64.zip
+        unzip tree-sitter-cli-linux-x64.zip
+        install -t $HOME/programs tree-sitter
+
+    )
+}
+VERSION_LATEST=$(curl -Ls -o /dev/null -w "%{url_effective}\n"  https://github.com/tree-sitter/tree-sitter/releases/latest)
+VERSION_LATEST=${VERSION_LATEST##*/}
+VERSION_LATEST=${VERSION_LATEST:1}
+
+echo $VERSION_LATEST
+
+if ! which tree-sitter >> /dev/null; then
+    fetch $VERSION_LATEST
+    return 
+fi
+
+current_version=$(tree-sitter --version | head -n 1 | cut -f2 -d' ')
+if [[ $VERSION_LATEST != $current_version ]]; then
+    echo installing due latest version ${VERSION_LATEST} current $current_version
+    fetch $VERSION_LATEST
+    return
+fi
+
+
+
+
+
